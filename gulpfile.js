41bffb206d0c37be3db45a2a648228e739295b09
Maris Usis <marisusisme@gmail.com>
2016-09-15 14:21:43 -0400

    styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var jade = require('gulp-jade');
 
 gulp.task('default', function() {
   // place code for your default task here
   console.log("Default gulp task");
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   return gulp.src('./client/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./client/css'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./client/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('templates', function() {
+  gulp.src('./client/*.jade')
+    .pipe(jade({
+      client: true
+    }))
+    .pipe(gulp.dest('./client/'))
 });
